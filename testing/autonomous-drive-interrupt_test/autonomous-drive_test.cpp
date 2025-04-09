0a972d503077056089a35c107126674680e51663
Patrick Harvey <patrick.s.harvey1@gmail.com>
2025-04-09 01:14:46 -0600

    fixed autonomous drive uart command processing

--- testing/autonomous-drive-interrupt_test/autonomous-drive_test.cpp
+++ testing/autonomous-drive-interrupt_test/autonomous-drive_test.cpp
@@ -20,56 +20,145 @@
 
 #define DRIVE_SPEED 30
 
-int cmd_chars_recv;
-uint8_t cmd[3];
+volatile int cmd_chars_recv = 0;
+volatile uint8_t cmd[5];
 
 
-void on_uart_rx() {
+void on_uart_rx() 
+{
     
     while (uart_is_readable(UART_ID)) {
         uint8_t cmd_char = uart_getc(UART_ID);
         cmd[cmd_chars_recv] = cmd_char;
         cmd_chars_recv++;
-        if (cmd_chars_recv == 3) {
-            if (cmd[0] == (uint8_t) 'D' && cmd[1] == (uint8_t) 'R') {
-                switch (cmd[2]) {
-                    case (uint8_t) 'F':
-                        setDriveSpeeds(DRIVE_SPEED, DRIVE_SPEED);
-                        printf("drive forward\n");
-                        break;
-                    case (uint8_t) 'B':
-                        setDriveSpeeds(-DRIVE_SPEED, -DRIVE_SPEED);
-                        printf("drive backward\n");
-                        break;
-                    case (uint8_t) 'L':
-                        setDriveSpeeds(-0.75*DRIVE_SPEED, 0.75*DRIVE_SPEED);
-                        printf("turn left\n");
-                        break;
-                    case (uint8_t) 'R':
-                        setDriveSpeeds(0.75*DRIVE_SPEED, -0.75*DRIVE_SPEED);
-                        printf("turn right\n");
-                        break;
-                    case (uint8_t) 'S':
-                        setDriveSpeeds(0,0);
-                        printf("halt\n");
-                        break;
-                    default:
-                        printf("invalid drive command, halting!\n");
-                        setDriveSpeeds(0,0);
+        printf("Recv %d, count = %d\n", cmd_char, cmd_chars_recv);
+        if (cmd[0] == (uint8_t) 'D') {
+            if (cmd[1] == (uint8_t) 'R' && cmd_chars_recv > 1) {
+                if (cmd_chars_recv > 2) {
+                    switch (cmd[2]) {
+                        case (uint8_t) 'F':
+                            setDriveSpeeds(DRIVE_SPEED, DRIVE_SPEED);
+                            printf("drive forward\n");
+                            cmd_chars_recv = 0;
+                            cmd[0] = 0;
+                            cmd[1] = 0;
+                            cmd[2] = 0;
+                            cmd[3] = 0;
+                            cmd[4] = 0;
+                            break;
+                        case (uint8_t) 'B':
+                            setDriveSpeeds(-DRIVE_SPEED, -DRIVE_SPEED);
+                            printf("drive backward\n");
+                            cmd_chars_recv = 0;
+                            cmd[0] = 0;
+                            cmd[1] = 0;
+                            cmd[2] = 0;
+                            cmd[3] = 0;
+                            cmd[4] = 0;
+                            break;
+                        case (uint8_t) 'L':
+                            setDriveSpeeds(-0.75*DRIVE_SPEED, 0.75*DRIVE_SPEED);
+                            printf("turn left\n");
+                            cmd_chars_recv = 0;
+                            cmd[0] = 0;
+                            cmd[1] = 0;
+                            cmd[2] = 0;
+                            cmd[3] = 0;
+                            cmd[4] = 0;
+                            break;
+                        case (uint8_t) 'R':
+                            setDriveSpeeds(0.75*DRIVE_SPEED, -0.75*DRIVE_SPEED);
+                            printf("turn right\n");
+                            cmd_chars_recv = 0;
+                            cmd[0] = 0;
+                            cmd[1] = 0;
+                            cmd[2] = 0;
+                            cmd[3] = 0;
+                            cmd[4] = 0;
+                            break;
+                        case (uint8_t) 'S':
+                            setDriveSpeeds(0,0);
+                            printf("halt\n");
+                            cmd_chars_recv = 0;
+                            cmd[0] = 0;
+                            cmd[1] = 0;
+                            cmd[2] = 0;
+                            cmd[3] = 0;
+                            cmd[4] = 0;
+                            break;
+                        default:
+                            printf("invalid drive command, halting!\n");
+                            setDriveSpeeds(0,0);
+                            cmd_chars_recv = 0;
+                            cmd[0] = 0;
+                            cmd[1] = 0;
+                            cmd[2] = 0;
+                            cmd[3] = 0;
+                            cmd[4] = 0;
+                    }
                 }
-            } else if (cmd[0] == (uint8_t) 'M' && cmd[1] == (uint8_t) 'R' && cmd[2] == (uint8_t) 'S') {
-                int8_t leftSpeedByte = uart_getc(UART_ID);
-                int8_t rightSpeedByte = uart_getc(UART_ID);
-                printf("motorset %d, %d\n", (int) leftSpeedByte, (int) rightSpeedByte);
-                setDriveSpeeds((int) leftSpeedByte, (int) rightSpeedByte);
-            } else {
-                printf("invalid drive command, halting!\n");
-                setDriveSpeeds(0,0);
+            
+            } else if (cmd_chars_recv > 1) {
+                cmd_chars_recv = 0;
+                cmd[0] = 0;
+                cmd[1] = 0;
+                cmd[2] = 0;
+                cmd[3] = 0;
+                cmd[4] = 0;
             }
-
+        } else if (cmd[0] == (uint8_t) 'M') {
+            if (cmd_chars_recv > 1 && cmd[1] == (uint8_t) 'R') {
+                if (cmd_chars_recv > 2 && cmd[2] == (uint8_t) 'S') {
+                    if (cmd_chars_recv > 3) {
+                        if (cmd_chars_recv > 4) {
+                            cmd_chars_recv++;
+                            printf("motorset %d, %d\n", (int) cmd[3], (int) cmd[4]);
+                            setDriveSpeeds((int) cmd[3], (int) cmd[4]);
+                            cmd_chars_recv = 0;
+                            cmd[0] = 0;
+                            cmd[1] = 0;
+                            cmd[2] = 0;
+                            cmd[3] = 0;
+                            cmd[4] = 0;
+                        } else  if (cmd_chars_recv > 4) {
+                            cmd_chars_recv = 0;
+                            cmd[0] = 0;
+                            cmd[1] = 0;
+                            cmd[2] = 0;
+                            cmd[3] = 0;
+                            cmd[4] = 0;
+                        }
+                    } else if (cmd_chars_recv > 3) {
+                        cmd_chars_recv = 0;
+                        cmd[0] = 0;
+                        cmd[1] = 0;
+                        cmd[2] = 0;
+                        cmd[3] = 0;
+                        cmd[4] = 0;
+                    }
+                } else if (cmd_chars_recv > 2) {
+                    cmd_chars_recv = 0;
+                    cmd[0] = 0;
+                    cmd[1] = 0;
+                    cmd[2] = 0;
+                    cmd[3] = 0;
+                    cmd[4] = 0;
+                }
+            } else if (cmd_chars_recv > 1) {
+                cmd_chars_recv = 0;
+                cmd[0] = 0;
+                cmd[1] = 0;
+                cmd[2] = 0;
+                cmd[3] = 0;
+                cmd[4] = 0;
+            }
+        } else {
+            cmd[0] = 0;
+            cmd[1] = 0;
+            cmd[2] = 0;
+            cmd[3] = 0;
+            cmd[4] = 0;
             cmd_chars_recv = 0;
-
-
         }
     }
 }
